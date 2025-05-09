04c8a86787047499bc33b0ca6836fad00f1a8d38
Tester <test@email.com>
2025-05-09 09:42:08 -0400

    commit

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,22 +1,6 @@
 export default function HeroHome() {
   return (
-    <section className="h-screen">
-      <div className="h-[300px] md:h-[50vh] relative">
-        <div 
-          className="absolute inset-0 bg-cover bg-center"
-          style={{
-            backgroundImage: "url('/images/hero2.jpeg')"
-          }}
-        >
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div className="text-center text-white">
-              <h1 className="h1 lg:text-6xl mb-4 font-red-hat-display font-black">Your website, reimagined</h1>
-              <p className="text-xl">Our landing page template works on all devices</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
+    <section className="md:h-screen">
       <div className="h-[300px] md:h-[50vh] relative">
         <div 
           className="absolute inset-0 bg-cover bg-center"
@@ -26,10 +10,28 @@ export default function HeroHome() {
         >
           <div className="absolute inset-0 flex items-center justify-center">
             <div className="text-center text-white">
-              <h2 className="h2 lg:text-5xl mb-4 font-red-hat-display font-black">Beautiful Results Forever</h2>
-              <a className="btn text-white bg-teal-500 hover:bg-teal-400" href="#0">Get Started</a>
+              <h1 className="h1 lg:text-6xl mb-4 font-red-hat-display font-black">Welcome to Mourning Spring</h1>
+              <p className="text-xl">play. . . sing . . . walk . . .& write through it all</p>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="h-[300px] md:h-[50vh] relative">
+        <div 
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: "url('/images/hero2.jpeg')"
+          }}
+        >
+          <div 
+            className="absolute inset-0 bg-center bg-no-repeat"
+            style={{
+              backgroundImage: "url('/images/poem.png')",
+              backgroundSize: "contain"
+            }}
+          />
+      
         </div>
       </div>
     </section>
